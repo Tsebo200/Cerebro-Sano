302593d367e1a10d9ed18e86cb5ff490e2004c6f
Tsebo200 <68067310+Tsebo200@users.noreply.github.com>
2022-05-23 01:48:14 +0200

    Sign up form Styled
    Preliminary Sign up form

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -7,10 +7,20 @@ const SignUp = () => {
         <>
         <div className='main-container'>
         <div className='sign-container'>
-            <div className='left'></div>
+            <div className='left'>
+                <p className='login-text'>Log in</p>
+                <p className='welcome-login-text'>Welcome back! Please enter your details</p>
+                <p className='email-text'>Email</p>
+                <form><input className='email-input' type={'text'} placeholder={"Enter Your Email"}></input></form>
+                <p className='password-text'>Password</p>
+                <form><input className='password-input' type={'text'} placeholder={"Enter Your Password"}></input></form>
+                <div className='btn-container'>
+                    <p className='signIn-btn'>Sign in</p>
+                </div>
+                <p className='no-account-text'>Don't have an account?</p><p className='signUp-text'>Sign up</p>
+            </div>
             <div className='right'></div>
         </div>
-
         </div>        
         </>
     );
