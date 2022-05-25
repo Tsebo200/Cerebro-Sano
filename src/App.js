ea60ba1ae41aebfc4371082e05e4e8118f505b42
Tsebo200 <68067310+Tsebo200@users.noreply.github.com>
2022-05-25 03:47:12 +0200

    Added Appointment cards
    Designed appointment cards and added a patients page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import SignUp from './components/SignUp';
 import Navigation from './components/Navigation';
 import Home from './components/Home';
+import Patient from './components/Patient';
 import {Route, Routes} from 'react-router-dom';
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
       <Route path="/" element={<Home/>} />
       <Route path="/signup" element={<SignUp />} />
+      <Route path="/patient" element={<Patient />} />
   
       </Routes>
     </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,82 @@ import React from "react";
 import moment from "moment";
 
 const Home = () => {
+    console.log(moment().format('DD MMM YYYY'))
+
     return(
-      console.log(moment().format('DD MMM YYYY'))
+        <>
+            <div className="header"></div>
+            <div className="main-container">
+                <div className="calendar-container"></div>
+                <div className="appointment-container">
+                    <div className="appoint-card-container">
+                        <div className="text-info-box">
+                            <p className="time">12:00</p>
+                            <p className="date">20/05/22</p>
+                            <p className="doctor">Dr Welsh</p>
+                            <p className="room">Room 5</p>
+                            <div className="profile-img"></div>
+                        </div>
+
+                    <div className="appoint-card-container-two">
+                        <div className="text-info-box-two">
+                            <p className="time">12:00</p>
+                            <p className="date">20/05/22</p>
+                            <p className="doctor">Dr Welsh</p>
+                            <p className="room">Room 5</p>
+                            <div className="profile-img-two"></div>
+                        </div>
+                    </div>
+
+                    <div className="appoint-card-container-three">
+                        <div className="text-info-box">
+                            <p className="time">12:00</p>
+                            <p className="date">20/05/22</p>
+                            <p className="doctor">Dr Welsh</p>
+                            <p className="room">Room 5</p>
+                            <div className="profile-img"></div>
+                        </div>
+                    </div>
+
+                    <div className="appoint-card-container-four">
+                        <div className="text-info-box">
+                            <p className="time">12:00</p>
+                            <p className="date">20/05/22</p>
+                            <p className="doctor">Dr Welsh</p>
+                            <p className="room">Room 5</p>
+                            <div className="profile-img"></div>
+                        </div>
+                    </div>
+
+                    <div className="appoint-card-container-five">
+                        <div className="text-info-box">
+                            <p className="time">12:00</p>
+                            <p className="date">20/05/22</p>
+                            <p className="doctor">Dr Welsh</p>
+                            <p className="room">Room 5</p>
+                            <div className="profile-img"></div>
+                        </div>
+                    </div>
+
+                    <div className="appoint-card-container-six">
+                        <div className="text-info-box">
+                            <p className="time">12:00</p>
+                            <p className="date">20/05/22</p>
+                            <p className="doctor">Dr Welsh</p>
+                            <p className="room">Room 5</p>
+                            <div className="profile-img"></div>
+                        </div>
+                    </div>
+
+
+
+
+                    </div>
+                </div>
+            </div>
+        </>
+     
+
     );
    
 }
